729ede2f79750c9492e2b114988b77d8d3870973
Lusca44 <lucasssrodrigues1@gmail.com>
2021-03-24 16:03:19 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,10 +33,17 @@ public class Program {
             System.out.println(obj);
         }
 
-        System.out.println("\n=== TESTE 4 : seller insert ===");
+        /*System.out.println("\n=== TESTE 4 : seller insert ===");
         Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com", new Date(), 4000.00, dep);
         sellerDao.insert(newSeller);
 
-        System.out.println("Inserted! new seller id = " + newSeller.getId());
+        System.out.println("Inserted! new seller id = " + newSeller.getId());*/
+
+        System.out.println("\n=== TESTE 5 : seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha");
+        sellerDao.update(seller);
+        System.out.println("UPDATED!");
+
     }
 }
