985b100f1f0a766360c5e40944093ce6d174d16b
Lusca44 <lucasssrodrigues1@gmail.com>
2021-03-24 18:59:47 -0300

    insert department implementation

---
+++ src/application/Program2.java
@@ -0,0 +1,25 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.util.Scanner;
+
+public class Program2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        System.out.println("=== TESTE 1 : department insert ===");
+
+        Department newDepartment = new Department(null, "Music");
+        departmentDao.insert(newDepartment);
+
+        System.out.println("New department inserted!");
+
+
+        sc.close();
+    }
+}
